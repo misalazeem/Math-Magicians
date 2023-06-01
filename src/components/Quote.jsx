8a4f3faecf6b74536846816dfa36ee8507e00446
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-06-01 18:40:02 +0500

    adding required changes

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,15 +2,31 @@ import { React, useState, useEffect } from 'react';
 
 function Quote() {
   const [data, setData] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [hasError, sethasError] = useState(false);
+
   useEffect(() => {
+    setisLoading(true);
     fetch('https://api.api-ninjas.com/v1/quotes', {
       headers: {
         'X-Api-Key': 'OpjST6lLVPMztqEXqvVpUQ==0mUc3zSnJ67pxMdw',
       },
     })
       .then((response) => response.json())
-      .then((apidata) => setData(apidata[0]));
+      .then((apidata) => {
+        setData(apidata[0]);
+        setisLoading(false);
+      })
+      .catch((_error) => {
+        sethasError(true);
+        setisLoading(false);
+        setData(_error);
+      });
   }, []);
+
+  if (hasError) return <div className="Quote">Something Went Wrong </div>;
+
+  if (isLoading) return <div className="Quote">Loading... </div>;
 
   return (
     <>
